152f915f5bd37d6632bc823f5d3f55ef94887e2e
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-20 08:41:10 -0300

    bugfix e npm start

--- routes/auth.js
+++ routes/auth.js
@@ -31,12 +31,16 @@ router.get('/logout', (req, res) => {
 
 router.post('/login', async (req, res) => {
   const user = await User.findOne({ username: req.body.userName })
-  const isValid = await user.checkPassword(req.body.password)
+  if (user) {
+    const isValid = await user.checkPassword(req.body.password)
 
-  if (isValid) {
-    req.session.user = user
-    req.session.role = user.roles[0]
-    res.redirect('/restrito/noticias')
+    if (isValid) {
+      req.session.user = user
+      req.session.role = user.roles[0]
+      res.redirect('/restrito/noticias')
+    } else {
+      res.redirect('/login')
+    }
   } else {
     res.redirect('/login')
   }
