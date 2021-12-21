a037ef3d5b1d260877f74bd557854a5b99ddfdd2
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-21 20:07:19 -0300

    PassportJS com Local Strategy

--- routes/auth.js
+++ routes/auth.js
@@ -2,21 +2,52 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+router.use(passport.initialize())
+router.use(passport.session())
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
+
+// definindo a estratégia para login local
+passport.use(new LocalStrategy(async (username, password, done) => {
+  const user = await User.findOne({ username })
+  if (user) {
+    const isValid = await user.checkPassword(password)
+    if (isValid) {
+      return done(null, user)
+    } else {
+      return done(null, false)
+    }
+  } else {
+    return done(null, false)
+  }
+}))
+
 router.use((req, res, next) => {
-  if ('user' in req.session) {
-    res.locals.user = req.session.user
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user
+    if (!req.session.role) {
+      req.session.role = req.user.roles[0]
+    }
     res.locals.role = req.session.role
   }
   next()
 })
 
 router.get('/change-role/:role', (req, res) => {
-  if (req.session.user.roles.indexOf(req.params.role) >= 0) {
-    if ('user' in req.session) {
+  if (req.isAuthenticated()) {
+    if (req.user.roles.indexOf(req.params.role) >= 0) {
       req.session.role = req.params.role
     }
+    res.redirect('/')
   }
-  res.redirect('/')
 })
 
 router.get('/login', (req, res) => {
@@ -29,21 +60,18 @@ router.get('/logout', (req, res) => {
   })
 })
 
-router.post('/login', async (req, res) => {
-  const user = await User.findOne({ username: req.body.userName })
-  if (user) {
-    const isValid = await user.checkPassword(req.body.password)
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: false
+}))
 
-    if (isValid) {
-      req.session.user = user
-      req.session.role = user.roles[0]
-      res.redirect('/restrito/noticias')
-    } else {
-      res.redirect('/login')
-    }
-  } else {
-    res.redirect('/login')
+router.get('/facebook', passport.authenticate('facebook'))
+router.get('/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  (req, res) => {
+    req.redirect('/')
   }
-})
+)
 
 module.exports = router
